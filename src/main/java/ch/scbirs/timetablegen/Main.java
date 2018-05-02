80a364c9669ebc79cad09583b08dddbfdf034c15
Tim Bachmann <tim.bachmann@stud.unibas.ch>
2018-05-02 14:15:09 +0200

    Fix not closing program after closing window

--- src/main/java/ch/scbirs/timetablegen/Main.java
+++ src/main/java/ch/scbirs/timetablegen/Main.java
@@ -15,7 +15,7 @@ public class Main {
         setUIFont();
         MainWindow window = new MainWindow();
         window.setVisible(true);
-        window.setDefaultCloseOperation(MainWindow.DISPOSE_ON_CLOSE);
+        window.setDefaultCloseOperation(MainWindow.EXIT_ON_CLOSE);
     }
 
     private static void setUIFont() {
